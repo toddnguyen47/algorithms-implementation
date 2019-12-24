6a6f11d4815c8964e4bfddab1d0cb8a157533a75
Todd Nguyen <todd.nguyen47@gmail.com>
2019-12-23 17:55:26 -0800

    Implemented addFirst() and its tests

--- java/doublyLinkedList/src/main/java/com/doublyLinkedList/DoublyLinkedList.java
+++ java/doublyLinkedList/src/main/java/com/doublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,53 @@
 package com.doublyLinkedList;
 
-public class DoublyLinkedList {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class DoublyLinkedList<T> {
+  private class Node {
+    public Node prev;
+    public Node next;
+    public T val;
+    public Node(T _val) {val = _val;}
+  }
+  
+  private int size;
+  private Node dummyHead;
+  private Node dummyTail;
+  
+  public int size() {return this.size;}
+  public T getFirst() {return this.dummyHead.next.val;}
+  public T getLast() {return this.dummyTail.prev.val;}
+
+  public Iterator<T> iterateHead() {return iterateElements(dummyHead);}
+  public Iterator<T> iterateTail() {return iterateElements(dummyTail);}
+
+  private Iterator<T> iterateElements(Node startingPtr) {
+    ArrayList<T> iter = new ArrayList<>();
+    while (startingPtr != null) {
+      if (startingPtr != dummyHead && startingPtr != dummyTail)
+        iter.add(startingPtr.val);
+      startingPtr = startingPtr.next;
+    }
+    return iter.iterator();
+  }
+
+  public DoublyLinkedList() {
+    this.size = 0;
+    this.dummyHead = new Node(null);
+    this.dummyTail = new Node(null);
+    dummyHead.next = dummyTail;
+    dummyTail.prev = dummyHead;
+  }
+
+  /** Add to the beginning of the Doubly Linked List. */
+  public void addFirst(T val) {
+    Node node = new Node(val);
+    node.next = dummyHead.next;
+    node.prev = dummyHead;
+
+    dummyHead.next.prev = node;
+    dummyHead.next = node;
+    this.size += 1;
+  }
 }
--- java/doublyLinkedList/src/test/java/com/doublyLinkedList/TestDoublyLinkedList.java
+++ java/doublyLinkedList/src/test/java/com/doublyLinkedList/TestDoublyLinkedList.java
@@ -2,12 +2,52 @@ package com.doublyLinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Iterator;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestDoublyLinkedList {
+  private DoublyLinkedList<Integer> dLinkedList;
+  @BeforeEach
+  public void setup() {
+    dLinkedList = new DoublyLinkedList<>();
+  }
+
   @Test
-  public void testNothing() {
-    DoublyLinkedList dLinkedList = new DoublyLinkedList();
-    assertNotNull(dLinkedList);
+  public void addFirstWhenEmpty() {
+    int val = 2;
+    dLinkedList.addFirst(val);
+    assertEquals(1, dLinkedList.size());
+    
+    Iterator<Integer> iter = dLinkedList.iterateHead();
+    while (iter.hasNext()) {
+      assertEquals(val, iter.next());
+    }
+  }
+  
+  @Test
+  public void addWhenThereIsOneElement() {
+    dLinkedList.addFirst(1);
+    dLinkedList.addFirst(2);
+    
+    assertEquals(2, dLinkedList.getFirst());
+    assertEquals(1, dLinkedList.getLast());
+    assertEquals(2, dLinkedList.size());
+  }
+  
+  @Test
+  public void addFirstFiveElements() {
+    int maxLen = 5;
+    int inputs[] = new int[maxLen];
+    for (int i = 0; i < maxLen; i++) {
+      inputs[i] = i;
+      dLinkedList.addFirst(i);
+    }
+    
+    Iterator<Integer> iter = dLinkedList.iterateHead();
+    int val = maxLen - 1;
+    while (iter.hasNext())
+      assertEquals(val--, iter.next());
   }
 }
