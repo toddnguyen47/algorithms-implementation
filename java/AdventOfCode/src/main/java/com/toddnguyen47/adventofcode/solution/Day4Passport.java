ef9deb723a8fd16ccc89ca45706199e48b30be1e
Todd Nguyen <todd.nguyen47@gmail.com>
2020-12-15 17:03:07 -0800

    WIP: Day 4 part 2

--- java/AdventOfCode/src/main/java/com/toddnguyen47/adventofcode/solution/Day4Passport.java
+++ java/AdventOfCode/src/main/java/com/toddnguyen47/adventofcode/solution/Day4Passport.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.toddnguyen47.adventofcode.CustomPrint;
 
@@ -46,6 +48,8 @@ public class Day4Passport implements ISolution {
       Passport passport = iter.next();
       if (passport.isPassportValid()) {
         count += 1;
+        System.out.println(passport);
+        System.out.println("---");
       }
     }
     CustomPrint.print("Valid Passwords: '%d'", count);
@@ -62,6 +66,11 @@ public class Day4Passport implements ISolution {
     public final String PID = "pid";
     public final String CID = "cid";
 
+    private Pattern digitPattern = Pattern.compile("\\d+");
+    private Pattern nondigitPattern = Pattern.compile("\\D+");
+    private Pattern colorPattern = Pattern.compile("#[0-9a-f]{6}");
+    private Pattern pidPattern = Pattern.compile("\\d{9}");
+
     public Passport(List<String> passportLines) {
       for (String s1 : passportLines) {
         String[] tokens = s1.split("\\s");
@@ -74,20 +83,97 @@ public class Day4Passport implements ISolution {
 
     @Override
     public String toString() {
-      return String.format(
-          "byr: '%s', iyr: '%s', eyr: '%s', hgt: '%s'\n"
-              + "hcl: '%s', ecl: '%s', passportId: '%s', countryId: '%s'",
-          this.infoMap.getOrDefault(this.BYR, ""), this.infoMap.getOrDefault(this.IYR, ""),
-          this.infoMap.getOrDefault(this.EYR, ""), this.infoMap.getOrDefault(this.HGT, ""),
-          this.infoMap.getOrDefault(this.HCL, ""), this.infoMap.getOrDefault(this.ECL, ""),
-          this.infoMap.getOrDefault(this.PID, ""), this.infoMap.getOrDefault(this.CID, ""));
+      return String.format("%s:%s %s:%s %s:%s %s:%s\n" + "%s:%s %s:%s %s:%s %s:%s", this.BYR,
+          this.infoMap.getOrDefault(this.BYR, ""), this.IYR,
+          this.infoMap.getOrDefault(this.IYR, ""), this.EYR,
+          this.infoMap.getOrDefault(this.EYR, ""), this.HGT,
+          this.infoMap.getOrDefault(this.HGT, ""), this.HCL,
+          this.infoMap.getOrDefault(this.HCL, ""), this.ECL,
+          this.infoMap.getOrDefault(this.ECL, ""), this.PID,
+          this.infoMap.getOrDefault(this.PID, ""), this.CID,
+          this.infoMap.getOrDefault(this.CID, ""));
     }
 
     public boolean isPassportValid() {
-      return this.infoMap.containsKey(this.BYR) && this.infoMap.containsKey(this.IYR)
-          && this.infoMap.containsKey(this.EYR) && this.infoMap.containsKey(this.HGT)
-          && this.infoMap.containsKey(this.HCL) && this.infoMap.containsKey(this.ECL)
-          && this.infoMap.containsKey(this.PID);
+      return this.isBirthYearValid() && this.isIssueYearValid() && this.isExpirationYearValid()
+          && this.isHeightValid() && this.isHairColorValid() && this.isEyeColorValid()
+          && this.isPid();
+    }
+
+    private boolean isBirthYearValid() {
+      if (!this.infoMap.containsKey(this.BYR))
+        return false;
+
+      int birthYear = Integer.valueOf(this.infoMap.get(this.BYR));
+      return 1920 <= birthYear && birthYear <= 2002;
+    }
+
+    private boolean isIssueYearValid() {
+      if (!this.infoMap.containsKey(this.IYR))
+        return false;
+
+      int issueYear = Integer.valueOf(this.infoMap.get(this.IYR));
+      return 2010 <= issueYear && issueYear <= 2020;
+    }
+
+    private boolean isExpirationYearValid() {
+      if (!this.infoMap.containsKey(this.EYR))
+        return false;
+
+      int expirationYear = Integer.valueOf(this.infoMap.get(this.EYR));
+      return 2020 <= expirationYear && expirationYear <= 2030;
+    }
+
+    private boolean isHeightValid() {
+      if (!this.infoMap.containsKey(this.HGT))
+        return false;
+
+      String height = this.infoMap.get(this.HGT);
+      Matcher digitMatcher = this.digitPattern.matcher(height);
+      if (!digitMatcher.find())
+        return false;
+      int heightInt = Integer.valueOf(digitMatcher.group(0));
+
+      Matcher heightUnitMatcher = this.nondigitPattern.matcher(height);
+      if (!heightUnitMatcher.find())
+        return false;
+      String unit = heightUnitMatcher.group(0);
+
+      if (unit.equals("cm")) {
+        return 150 <= heightInt && heightInt <= 193;
+      } else if (unit.equals("in")) {
+        return 59 <= heightInt && heightInt <= 76;
+      } else {
+        return false;
+      }
+    }
+
+    private boolean isHairColorValid() {
+      if (!this.infoMap.containsKey(this.HCL))
+        return false;
+
+      String hairColor = this.infoMap.get(this.HCL);
+      Matcher m = this.colorPattern.matcher(hairColor);
+      return m.find();
+    }
+
+    private boolean isEyeColorValid() {
+      if (!this.infoMap.containsKey(this.ECL))
+        return false;
+
+      String eyeColor = this.infoMap.get(this.ECL);
+      return eyeColor.equals("amb") || eyeColor.equals("blu") || eyeColor.equals("brn")
+          || eyeColor.equals("gry") || eyeColor.equals("grn") || eyeColor.equals("hzl")
+          || eyeColor.equals("oth");
+    }
+
+    private boolean isPid() {
+      if (!this.infoMap.containsKey(this.PID))
+        return false;
+
+      String pid = this.infoMap.get(this.PID);
+      Matcher m = this.pidPattern.matcher(pid);
+      return m.find();
     }
   }
 }
