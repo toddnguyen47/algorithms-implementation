9a7e68c7bcc6012cc4a7435dfcb3fdfdd7e12afe
Todd Nguyen <todd.nguyen47@gmail.com>
2021-03-31 19:39:39 -0700

    Got quicksort with lomuto partition to work!

--- rust/dependency-inversion/sorting-algos/src/lib.rs
+++ rust/dependency-inversion/sorting-algos/src/lib.rs
@@ -14,6 +14,8 @@
 //! }
 //!```
 
+use std::fmt::Debug;
+
 pub mod factory;
 pub mod i_sorting;
 
@@ -21,7 +23,10 @@ mod merge_sort;
 mod quicksort;
 
 /// Sort the `arr` using `sorting_algorithm` created by `factory`
-pub fn sort<T>(sorting_algorithm: i_sorting::Sorting, arr: &mut [T]) -> &mut [T] {
+pub fn sort<T>(sorting_algorithm: i_sorting::Sorting, arr: &mut [T]) -> &mut [T]
+where
+    T: core::cmp::PartialOrd + core::marker::Copy + Debug,
+{
     let arr = match sorting_algorithm {
         i_sorting::Sorting::MergeSort(merge_sort) => merge_sort.sort(arr),
         i_sorting::Sorting::Quicksort(quicksort) => quicksort.sort(arr),
@@ -32,12 +37,53 @@ pub fn sort<T>(sorting_algorithm: i_sorting::Sorting, arr: &mut [T]) -> &mut [T]
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
+    use rand::prelude::*;
+    use rand_pcg::Pcg64;
+
+    fn generate_n_random_elements(n: usize) -> Vec<usize> {
+        let mut rng = Pcg64::from_rng(rand::thread_rng()).unwrap();
+        let mut vec = Vec::with_capacity(n);
+
+        for _ in 0..n {
+            vec.push(rng.gen_range(0..(n / 2)));
+        }
+
+        vec
+    }
 
     #[test]
     fn able_to_sort_5_unsorted_elements() {
         let mut arr = [14, 2, 44, 3, 7];
-        let algo = crate::factory::create_merge_sort();
+        let algo = crate::factory::create_quicksort();
         let sorted_arr = crate::sort(algo, &mut arr);
         assert_eq!([2, 3, 7, 14, 44], sorted_arr);
+    }
+
+    #[test]
+    fn able_to_sort_5_sorted_elements() {
+        let mut arr = [2, 3, 7, 14, 44];
+        let algo = crate::factory::create_quicksort();
+        let sorted_arr = crate::sort(algo, &mut arr);
+        assert_eq!([2, 3, 7, 14, 44], sorted_arr);
+    }
+
+    #[test]
+    fn able_to_sort_20_random_elements() {
+        let mut arr = generate_n_random_elements(20);
+        let mut arr_clone = arr.clone();
+        let algo = crate::factory::create_quicksort();
+        let sorted_arr = crate::sort(algo, &mut arr_clone);
+        arr.sort_unstable();
+        assert_eq!(arr, sorted_arr);
+    }
+
+    #[test]
+    fn able_to_sort_10_000_random_elements() {
+        let mut arr = generate_n_random_elements(10_000);
+        let mut arr_clone = arr.clone();
+        let algo = crate::factory::create_quicksort();
+        let sorted_arr = crate::sort(algo, &mut arr_clone);
+        arr.sort_unstable();
+        assert_eq!(arr, sorted_arr);
     }
 }
--- rust/dependency-inversion/sorting-algos/src/quicksort.rs
+++ rust/dependency-inversion/sorting-algos/src/quicksort.rs
@@ -5,8 +5,77 @@ impl Quicksort {
         Self
     }
 
-    pub fn sort<'a, T>(&self, arr: &'a mut [T]) -> &'a mut [T] {
-        println!("I am in quicksort!");
+    pub fn sort<'a, T>(&self, arr: &'a mut [T]) -> &'a mut [T]
+    where
+        T: core::cmp::PartialOrd + core::marker::Copy + core::fmt::Debug,
+    {
+        self.sort_helper(arr, 0, arr.len() - 1)
+    }
+
+    fn sort_helper<'a, T>(&self, mut arr: &'a mut [T], lo: usize, hi: usize) -> &'a mut [T]
+    where
+        T: core::cmp::PartialOrd + core::marker::Copy + core::fmt::Debug,
+    {
+        if lo < hi {
+            let (partitioned_arr, index_of_pivot) = self.lomuto_partition(arr, lo, hi);
+            arr = partitioned_arr;
+            arr = self.sort_helper(arr, lo, index_of_pivot);
+            arr = self.sort_helper(arr, index_of_pivot + 1, hi);
+        }
         arr
+    }
+
+    fn lomuto_partition<'a, T>(
+        &self,
+        arr: &'a mut [T],
+        lo: usize,
+        hi: usize,
+    ) -> (&'a mut [T], usize)
+    where
+        T: core::cmp::PartialOrd + core::marker::Copy + core::fmt::Debug,
+    {
+        self.move_median_of_three_pivot_to_hi(arr, lo, hi);
+        let pivot = arr[hi];
+        let mut desired_pivot_index = lo;
+        for i in lo..hi {
+            if arr[i] < pivot {
+                self.swap(arr, desired_pivot_index, i);
+                desired_pivot_index += 1;
+            }
+        }
+
+        // We found the desired index for our pivot at [hi], so
+        // Switch desired pivot index with hi!
+        self.swap(arr, desired_pivot_index, hi);
+        (arr, desired_pivot_index)
+    }
+
+    fn move_median_of_three_pivot_to_hi<'a, T>(&self, arr: &'a mut [T], lo: usize, hi: usize)
+    where
+        T: core::cmp::PartialOrd + core::marker::Copy + core::fmt::Debug,
+    {
+        let mid: usize = (lo + hi) / 2;
+        // Move lowest to [lo]
+        if arr[mid] < arr[lo] {
+            self.swap(arr, mid, lo);
+        }
+        if arr[hi] < arr[lo] {
+            self.swap(arr, hi, lo);
+        }
+        // Move highest to mid now. Median should be at 'hi'!
+        if arr[hi] > arr[mid] {
+            self.swap(arr, hi, mid);
+        }
+    }
+
+    fn swap<'a, T>(&self, arr: &'a mut [T], lo: usize, hi: usize)
+    where
+        T: Copy,
+    {
+        if lo != hi {
+            let temp = arr[lo];
+            arr[lo] = arr[hi];
+            arr[hi] = temp;
+        }
     }
 }
